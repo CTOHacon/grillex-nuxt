213844d260fd886c1531dcf779b3e48575a5d5cc
AlexanderBorysenko <borysenko.alexander@gmail.com>
2024-10-31 18:19:15 +0200

    feat: preload custom fonts and define font-face for AA Duke and Mulish

--- app.vue
+++ app.vue
@@ -39,15 +39,46 @@ useHead({
 			crossorigin: 'anonymous'
 		},
 		{
-			rel: 'stylesheet',
-			href: 'https://fonts.googleapis.com/css2?family=Mulish:wght@200..1000&display=swap'
+			rel: 'preload',
+			as: 'font',
+			href: '/fonts/AADuke-Fill.woff2',
+			type: 'font/woff2',
+			crossorigin: 'anonymous'
 		},
 		{
 			rel: 'preload',
-			href: '/AADuke-Fill.woff2',
 			as: 'font',
+			href: '/fonts/Mulish-cyr.woff2',
 			type: 'font/woff2',
 			crossorigin: 'anonymous'
+		},
+		{
+			rel: 'preload',
+			as: 'font',
+			href: '/fonts/Mulish-latin.woff2',
+			type: 'font/woff2',
+			crossorigin: 'anonymous'
+		}
+	],
+	style: [
+		{
+			innerHTML: `
+@font-face {font-family: 'AA Duke'; src: url('/fonts/AADuke-Fill.woff2') format('woff2-variations');font-display: swap;font-weight: 100 800;}
+@font-face {
+  font-family: 'Mulish';
+  font-style: normal;
+  font-weight: 200 1000;
+  src: url('/fonts/Mulish-cyr.woff2') format('woff2');
+  unicode-range: U+0301, U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+}
+@font-face {
+  font-family: 'Mulish';
+  font-style: normal;
+  font-weight: 200 1000;
+  src: url('/fonts/Mulish-latin.woff2') format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+`
 		}
 	]
 });
